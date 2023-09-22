da270eaa322e2ddd665804de6f7b4149494ba38e
Abhishek Agarwal <>
2023-09-22 09:35:25 +0000

    added NER encoding to encoder output

--- src/run.sh
+++ src/run.sh
@@ -15,7 +15,45 @@ python src/translator.py  train --device cuda:0 --maxlen 50 \
                                                 --subset 0 \
                                                 --batch_size 72 \
                                                 --num_epochs 20 \
-                                                --save_prefix runs/en_de/log0 \
-                                                --resume_dir runs/en_de/smallTrain_maxlen100_subset25k/ \
+                                                --save_prefix runs/en_de/log1 \
+                                                --resume_dir runs/en_de/log0_maxlen50_subset0/ \
                                                 --modelOnly_resume \
-                                                --scheduler_freq 200
+                                                --scheduler_freq 200 \
+                                                --min_lr 2e-6
+
+## NER
+# overfit
+python src/translator_v1.py  train --device cuda:1 --maxlen 50 \
+                                               --subset 240 \
+                                               --batch_size 72 \
+                                               --num_epochs 50 \
+                                               --save_prefix None \
+                                               --resume_dir None \
+                                               --init_lr 2e-4
+
+# fit small train
+python src/translator_v1.py  train --device cuda:1 --maxlen 50 \
+                                               --subset 25000 \
+                                               --batch_size 72 \
+                                               --num_epochs 50 \
+                                               --save_prefix runs/en_de/nerSmallTrain \
+                                               --resume_dir None \
+                                               --init_lr 2e-4
+
+# FullTrain
+python src/translator_v1.py  train --device cuda:1 --maxlen 50 \
+                                               --subset 0 \
+                                               --batch_size 200 \
+                                               --num_epochs 50 \
+                                               --save_prefix runs/en_de/ner_log0 \
+                                               --resume_dir runs/en_de/nerSmallTrain_maxlen50_subset25k/ \
+                                               --init_lr 2e-4
+
+python src/translator_v1.py  eval --device cuda:0 --maxlen 50 \
+                                               --subset 0 \
+                                               --batch_size 72 \
+                                               --num_epochs 50 \
+                                               --save_prefix None \
+                                               --resume_dir runs/en_de/ner_log0_maxlen50_subset0/ \
+                                               --init_lr 2e-4
+
--- src/translator.py
+++ src/translator.py
@@ -19,67 +19,124 @@ from torchmetrics.text import BLEUScore
 
 from datasets import load_dataset
 import transformers
-from transformers import AutoTokenizer, BertTokenizer, EncoderDecoderModel
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from transformers import AutoTokenizer, BertTokenizer, EncoderDecoderModel, AutoModelForTokenClassification
 
 from utils import collate_tokens, save_model
 from dataset import TransformerDataset
 
+## Transformer
+class Translator(nn.Module):
+    def __init__(self, model_params, toLang_tokenizer):
+        super(Translator, self).__init__()
+        ## Build Enc-Dec Model 
+        self.enc_dec = EncoderDecoderModel.from_encoder_decoder_pretrained(model_params['modelpath_from'], model_params['modelpath_to'])
 
-def build_model(model_params, device, resume_file = None):
-    ## Tokenizers
-    fromLang_tokenizer = BertTokenizer.from_pretrained(model_params['modelpath_from'])
-    toLang_tokenizer = AutoTokenizer.from_pretrained(model_params['modelpath_to'])
+        self.enc_dec.config.decoder_start_token_id = toLang_tokenizer.cls_token_id 
+        self.enc_dec.config.pad_token_id = toLang_tokenizer.pad_token_id 
 
-    ## Build Model 
-    model = EncoderDecoderModel.from_encoder_decoder_pretrained(model_params['modelpath_from'], model_params['modelpath_to'])
+        # Build NER model
+        self.ner = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
-    model.config.decoder_start_token_id = toLang_tokenizer.cls_token_id 
-    model.config.pad_token_id = toLang_tokenizer.pad_token_id 
-    model = model.to(device)
+        def freeze_module(module):
+            for param in module.parameters():
+                param.requires_grad_(False)
 
-    def freeze_module(module):
-        for param in module.parameters():
-            param.requires_grad_(False)
+        # freeze layers of Encoder Decoder Model
+        freeze_module(self.enc_dec.encoder)
+        if not model_params['freeze_encOnly']:
+            freeze_module(self.enc_dec.decoder.bert.embeddings)
+            if not model_params['freeze_decEmbedOnly']:
+                for layer in self.enc_dec.decoder.bert.encoder.layer:
+                    freeze_module(layer.attention)
+        else:
+            assert not model_params['freeze_decEmbedOnly'], "specify only one of `freeze_decEmbedOnly` or `freeze_encOnly`"
 
-    freeze_module(model.encoder)
-    if not model_params['freeze_encOnly']:
-        freeze_module(model.decoder.bert.embeddings)
-        if not model_params['freeze_decEmbedOnly']:
-            for layer in model.decoder.bert.encoder.layer:
-                freeze_module(layer.attention)
-    else:
-        assert not model_params['freeze_decEmbedOnly'], "specify only one of `freeze_decEmbedOnly` or `freeze_encOnly`"
+        # freeze NER model
+        freeze_module(self.ner)
 
-    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=model_params['init_lr'])
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=model_params['gamma'], patience=model_params['patience'], min_lr= model_params['min_lr'],verbose=True)
+        # Linear Layer to shrink Encoder LLM output
+        self.shrink_enc_out = nn.Linear(self.enc_dec.encoder.config.hidden_size, 
+                                        self.enc_dec.decoder.config.hidden_size-len(self.ner.config.label2id))
 
-    ## Load model and optimizer from resume_dir
-    x0 = 0
-    if resume_file:
-        print(f"LOADING init model from {resume_file}..")
-        assert os.path.exists(resume_file)
-        resume_dict = torch.load(resume_file, map_location=device) 
+    @torch.no_grad()
+    def decode(self, input_ids, attention_mask, max_length=25):
+        """
+        for inference
+        Args:
+            src: input to encoder 
+            trg: input to decoder
+        out:
+            out_labels : returns final prediction of sequence
+        """
+        
+        input_device = input_ids.device
 
-        model.load_state_dict(resume_dict['model_state_dict'])
-        if not model_params['modelOnly_resume']:
-            optimizer.load_state_dict(resume_dict['optimizer_state_dict'])
-            scheduler.load_state_dict(resume_dict['scheduler_state_dict'])
-        x0 = resume_dict['x0']
-    print(f"init LEARNING RATE: {optimizer.param_groups[0]['lr']}")
+        ner_classes = F.softmax(self.ner(input_ids).logits, dim=-1)
 
+        enc_output = self.enc_dec.encoder(input_ids=input_ids, 
+                                          attention_mask=attention_mask, 
+                                          return_dict=True)[0]
+        shrunk_enc_out = self.shrink_enc_out(enc_output)
 
-    return fromLang_tokenizer, toLang_tokenizer, model, optimizer, scheduler, x0
+        encoder_hidden_states_extended = torch.cat((ner_classes, shrunk_enc_out), dim=-1)
+
+        out_labels = torch.tensor([[self.enc_dec.config.decoder_start_token_id]]*input_ids.shape[0]).to(input_device)
+        for _ in range(max_length):
+            dec_output = self.enc_dec.decoder(input_ids = out_labels, 
+                                              encoder_hidden_states=encoder_hidden_states_extended, 
+                                              encoder_attention_mask=attention_mask, 
+                                              return_dict=True).logits
+
+            out_labels = torch.cat((out_labels, dec_output[:,-1:,:].argmax(-1)),dim=1)
+
+        return out_labels
+
+    def forward(self, input_ids, attention_mask, decoder_input_ids,decoder_attention_mask):
+
+        ner_classes = F.softmax(self.ner(input_ids).logits, dim=-1)
+
+        encoder_hidden_states = self.enc_dec.encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=True
+        )[0]
+
+        # shrunk enc output and add one hot encoding of NER classes
+        shrunk_enc_out = self.shrink_enc_out(encoder_hidden_states)
+        encoder_hidden_states_extended = torch.cat((ner_classes, shrunk_enc_out), dim=-1)
+
+        decoder_outputs = self.enc_dec.decoder(
+            input_ids=decoder_input_ids,
+            attention_mask=decoder_attention_mask,
+            encoder_hidden_states=encoder_hidden_states_extended,
+            encoder_attention_mask=attention_mask,
+            inputs_embeds=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            use_cache=None,
+            past_key_values=None,
+            return_dict=True
+        )
+
+        return decoder_outputs.logits
 
 
 @torch.no_grad()
-def eval(model, test_loader, maxlen, toLang_tokenizer, nbeams=20, num_returns=3, limit=20):
-    bleu = BLEUScore(n_gram=3)
+def eval(model, test_loader, maxlen, toLang_tokenizer, limit=20):
+    model.eval()
+    f = lambda color: lambda string, **kwargs: print(color + string + "\33[0m", **kwargs)
+    p_red = f("\33[31m")
 
+    device_model = next(model.parameters()).device
     count = 0
     bleu_score = 0
+    max_ngrams = 3
+    bleu = BLEUScore(n_gram=max_ngrams)
     for i, data in enumerate(tqdm(test_loader, desc=f'eval', leave=False)):
-        out = model.generate(data['lang_from']['input_ids'].to(model.device), max_length=maxlen, num_beams=nbeams, num_return_sequences=num_returns, do_sample=False)
+        out = model.decode(input_ids=data['lang_from']['input_ids'].to(device_model), attention_mask=data['lang_from']['attention_mask'].to(device_model), max_length=maxlen)
 
         sep_idx = torch.argmax((out == toLang_tokenizer.sep_token_id).to(dtype=torch.int), dim=-1).tolist()
         out_text = []
@@ -91,24 +148,51 @@ def eval(model, test_loader, maxlen, toLang_tokenizer, nbeams=20, num_returns=3,
 
         for idx in range(target.shape[0]):
             target_idx = toLang_tokenizer.decode(target[idx,1:sep_idx[idx]])
-            out_idx = out_text[idx*num_returns:(idx+1)*num_returns]
-            bleu_score += bleu(out_idx,[[target_idx]])
-            if count<=limit:
-                print(target_idx)
-                print(out_idx)
-                print()
-            count += 1
-    return bleu_score/len(test_loader)
+            if len(target_idx.split())>=max_ngrams:
+                out_idx = out_text[idx]
+                bleu_idx = bleu([out_idx],[[target_idx]])
+                bleu_score += bleu_idx
+                if count<=limit:
+                    print(target_idx)
+                    print(out_idx)
+                    print()
+                count += 1
+    return bleu_score/count
 
 
-def train_one_epoch(model, optimizer, scheduler, scheduler_freq, loss_fn, train_loader, epoch, save_freq, log_writer = None, x0=0, running_loss=0, model_savefmt=None):
+def build_model(model_params, device, resume_file = None):
+    ## Tokenizers
+    fromLang_tokenizer = BertTokenizer.from_pretrained(model_params['modelpath_from'])
+    toLang_tokenizer = AutoTokenizer.from_pretrained(model_params['modelpath_to'])
+
+    model = Translator(model_params, toLang_tokenizer).to(device)
+
+    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=model_params['init_lr'])
+
+    ## Load model and optimizer from resume_dir
+    x0 = 0
+    if resume_file:
+        print(f"LOADING init model from {resume_file}..")
+        assert os.path.exists(resume_file)
+        resume_dict = torch.load(resume_file, map_location=device) 
+
+        model.load_state_dict(resume_dict['model_state_dict'])
+        if not model_params['modelOnly_resume']:
+            optimizer.load_state_dict(resume_dict['optimizer_state_dict'])
+        x0 = resume_dict['x0']
+    print(f"init LEARNING RATE: {optimizer.param_groups[0]['lr']}")
+
+    return fromLang_tokenizer, toLang_tokenizer, model, optimizer, x0
+
+
+
+def train_one_epoch(model, optimizer, loss_fn, train_loader, epoch, save_freq, log_writer = None, x0=0, model_savefmt=None):
     model.train()
     epoch_loss = 0
-    device = model.device
+    device = next(model.parameters()).device
 
     num_train_batches = len(train_loader)
     pbar = tqdm(train_loader, desc=f'epoch {epoch}', leave=False)
-    pbar.set_postfix(loss=f'{running_loss/scheduler_freq:.2f}', lr = f"{optimizer.param_groups[0]['lr']:.1e}")
     for i, data in enumerate(pbar):
         step = x0 + epoch * len(train_loader) + i + 1
         
@@ -119,32 +203,24 @@ def train_one_epoch(model, optimizer, scheduler, scheduler_freq, loss_fn, train_
         out_masks = data['lang_to']['attention_mask'].to(device)
 
         optimizer.zero_grad()
-        # decoder_input_ids = shift_tokens_right(out_tokens, model.config.pad_token_id, model.config.decoder_start_token_id).to(device)
-        out = model(input_ids=in_tokens, attention_mask = in_masks, decoder_input_ids=out_tokens,labels=out_tokens,decoder_attention_mask=out_masks)
-        predictions = out[1]
-        # predictions = F.log_softmax(predictions, dim=-1)
+        predictions = model(input_ids=in_tokens, attention_mask = in_masks, decoder_input_ids=out_tokens,decoder_attention_mask=out_masks)
         loss = loss_fn(predictions[:, :-1, :].contiguous().view(-1, predictions.shape[-1]), out_tokens[:, 1:].contiguous().view(-1))
         loss.backward()
         # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
 
-        running_loss += loss.item()
         epoch_loss += loss.item()
-
-        if (step-x0)%scheduler_freq == 0:
-            scheduler.step(running_loss/scheduler_freq)
-            pbar.set_postfix(loss=f'{running_loss/scheduler_freq:.2f}', lr = f"{optimizer.param_groups[0]['lr']:.1e}")
-            running_loss = 0
+        pbar.set_postfix(loss=f'{loss.item():.2f}')
 
         if model_savefmt and (step-x0)%save_freq == 0:
-            save_model(step, model, optimizer, scheduler, model_savefmt.format(epoch,i+1))
+            save_model(step, model, optimizer, model_savefmt.format(epoch,i+1))
             old_files = natsorted(glob.glob(model_savefmt.format('*','*')))[:-1]
             list(map(os.remove,old_files))
 
         if log_writer:
             log_writer.add_scalar('training loss', loss.item(), step)
 
-    return epoch_loss / num_train_batches, running_loss
+    return epoch_loss / num_train_batches
 
 
 def main(mode, model_params, data_params, train_params):
@@ -156,7 +232,7 @@ def main(mode, model_params, data_params, train_params):
     ## Build Model
     device = torch.device(train_params['device'] if torch.cuda.is_available() else "cpu")
     resume_file = natsorted(glob.glob(os.path.join(train_params['resume_dir'],train_params['save_format'].format('*','*'))))[-1] if train_params['resume_dir'] else None
-    fromLang_tokenizer, toLang_tokenizer, model, optimizer, scheduler, x0 = build_model(model_params, device, resume_file=resume_file)
+    fromLang_tokenizer, toLang_tokenizer, model, optimizer, x0 = build_model(model_params, device, resume_file=resume_file)
     x0 = x0 if train_params['x0']<0 else train_params['x0']
 
     ## Data
@@ -176,22 +252,21 @@ def main(mode, model_params, data_params, train_params):
         writer = SummaryWriter(train_params['save_prefix']) if train_params['save_prefix'] else None
 
         loss_fn = nn.CrossEntropyLoss(ignore_index=toLang_tokenizer.pad_token_id)
-        running_loss = 0
         for epoch in range(train_params['num_epochs']):
-            train_epoch_loss, running_loss = train_one_epoch(model, optimizer, scheduler, train_params['scheduler_freq'], loss_fn, train_loader, epoch, train_params['save_freq'],
-                                                             log_writer=writer, x0=x0, running_loss=running_loss, model_savefmt=model_savefmt)
+            train_epoch_loss = train_one_epoch(model, optimizer, loss_fn, train_loader, epoch, train_params['save_freq'],
+                                                             log_writer=writer, x0=x0, model_savefmt=model_savefmt)
             print("epoch {epoch:>{width}}: train_loss {train_loss:.3f}".format(epoch=epoch,train_loss=train_epoch_loss,width=3))
 
             if train_params['save_prefix']:
-                save_model(x0 + epoch * len(train_loader), model, optimizer, scheduler, model_savefmt.format(epoch,'N'))
+                save_model(x0 + epoch * len(train_loader), model, optimizer, model_savefmt.format(epoch,'N'))
                 old_files = natsorted(glob.glob(os.path.join(train_params['save_prefix'],train_params['save_format'].format('*','*'))))[:-1]
                 list(map(os.remove,old_files))
 
         if writer:
             writer.close()
-            
+    
     ## Eval
-    print("bleu score", eval(model, test_loader, train_params['maxlen'], toLang_tokenizer, nbeams=20, num_returns=3))
+    print("bleu score:", eval(model, test_loader, train_params['maxlen'], toLang_tokenizer))
 
 if __name__ == "__main__":
     model_params = {
@@ -201,14 +276,10 @@ if __name__ == "__main__":
 
                     'freeze_encOnly' : False,
                     'freeze_decEmbedOnly' : False,
-                    'skipScheduler' : False,
                     'modelOnly_resume' : False,
 
-                    # optimizer & schduler options
+                    # optimizer options
                     'init_lr' : 2e-4,
-                    'min_lr' : 1e-6,
-                    'gamma' : 0.5,
-                    'patience' : 2
                  }
 
     data_params = { 
@@ -225,7 +296,6 @@ if __name__ == "__main__":
                     # training options
                     'batch_size' : 24,
                     'num_epochs' : 30,
-                    'scheduler_freq' : 12,
                     'device' : 'cuda:0',
 
                     # checkpoint options
